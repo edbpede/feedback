626fa70127d147eb8c50b0b150240f5627234528
edbpede <144238562+edbpede@users.noreply.github.com>
2025-12-12 16:20:32 +0100

    feat: configure vercel adapter for static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import UnoCSS from 'unocss/astro';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [
     solidJs(),
     UnoCSS({
