bed049f3c56cfc09cbdf77babde9f9c4bba5d103
edbpede <144238562+edbpede@users.noreply.github.com>
2025-12-14 02:14:12 +0100

    feat: add prettier and configure for astro and tailwind
    This commit introduces Prettier for code formatting, along with specific plugins for Astro and Tailwind CSS.
    It also adds a `.prettierignore` file to exclude build outputs, dependencies, lock files, cache, IDE configurations, environment files, and generated files from formatting.
    Additionally, NPM scripts `format` and `format:check` are included for convenient formatting.

---
+++ prettier.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
